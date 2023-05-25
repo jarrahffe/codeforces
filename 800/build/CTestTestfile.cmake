4033b7fa8e2abd757806d776a3b10cc8339fb32a
jarraha <81581534+jarrahffe@users.noreply.github.com>
2023-05-26 09:58:54 +1000

    Delete 800/build directory

--- 800/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/jarjar/Documents/UNSW/codeforces/800
-# Build directory: /Users/jarjar/Documents/UNSW/codeforces/800/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
